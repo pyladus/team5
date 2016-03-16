93d4dd0dc9c7b6b698717573f977131f6b294730
Ferdinand Scheubeck <ferdinand.scheubeck@sap.com>
2016-03-16 15:38:58 +0000

    chande

--- view/Controller.controller.js
+++ view/Controller.controller.js
@@ -1,50 +1,50 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ], function(Controller, JSONModel) {
-	"use strict";
+    "use strict";
 
-	return Controller.extend("Refugee_Book.view.Controller", {
+    return Controller.extend("Refugee_Book.view.Controller", {
 
-		onInit: function() {
-			var oModel = new JSONModel("DataGroup.json");
+        onInit : function () {
+            var oModel = new JSONModel("DataGroup.json");
 
-			this.getView().setModel(oModel);
+            this.getView().setModel(oModel);
+        },
+        
+        onNavButtonPress: function() {
+		window.history.go(-1);
 		},
-
-		onNavButtonPress: function() {
-			window.history.go(-1);
+		
+		press : function(evt) {
+		alert("The generic tile is pressed.");
 		},
-
-		press: function(evt) {
-			alert("The generic tile is pressed.");
+		press1 : function(evt) {
+		sap.ui.core.UIComponent.getRouterFor(this).navTo("Person");
 		},
-
+ 
 		handleLogoffPress: function(oEvent) {
 			sap.m.MessageToast.show("Logoff Button Pressed");
 		},
-		handlePressMyProfile: function(oEvent) {
-							sap.ui.core.UIComponent.getRouterFor(this).navTo("MyProfile");
-		},
-
+ 
 		handleUserItemPressed: function(oEvent) {
 			sap.m.MessageToast.show("User Button Pressed");
 		},
-
+ 
 		handleSearchItemSelect: function(oEvent) {
 			sap.m.MessageToast.show("Search Entry Selected: " + oEvent.getSource().getTitle());
 		},
-
+ 
 		handleShellOverlayClosed: function() {
-			sap.m.MessageToast.show("Overlay closed");
+		sap.m.MessageToast.show("Overlay closed");
 		},
-
+ 
 		handleSearchPressed: function(oEvent) {
 			var sQuery = oEvent.getParameter("query");
-			if (sQuery == "") {
+			if(sQuery == "") {
 				return;
 			}
-
+ 
 			// create Overlay only once
 			if (!this._overlay) {
 				this._overlay = sap.ui.xmlfragment(
@@ -53,37 +53,39 @@ sap.ui.define([
 				);
 				this.getView().addDependent(this._overlay);
 			}
-
+ 
 			// mock data
 			var aResultData = [];
-			for (var i = 0; i < 10; i++) {
+			for(var i = 0; i < 10; i++) {
 				aResultData.push({
-					title: (i + 1) + ". " + sQuery,
-					text: "Lorem ipsum sit dolem"
-				});
+									title:(i + 1) + ". " + sQuery,
+									text:"Lorem ipsum sit dolem"
+								});
 			}
 			var oData = {
-				searchFieldContent: sQuery,
-				resultData: aResultData
-			};
+							searchFieldContent: sQuery,
+							resultData: aResultData
+						};
 			var oModel = new JSONModel();
 			oModel.setData(oData);
 			this._overlay.setModel(oModel);
-
+ 
 			// set reference to shell and open overlay
 			this._overlay.setShell(this.getView().byId("myShell"));
 			this._overlay.open();
 		},
 
-		onCollapseAll: function() {
-			var oTreeTable = this.getView().byId("TreeTableBasic");
-			oTreeTable.collapseAll();
-		},
+        onCollapseAll: function () {
+            var oTreeTable = this.getView().byId("TreeTableBasic");
+            oTreeTable.collapseAll();
+        },
 
-		onExpandFirstLevel: function() {
-			var oTreeTable = this.getView().byId("TreeTableBasic");
-			oTreeTable.expandToLevel(1);
-		}
-	});
+        onExpandFirstLevel: function () {
+            var oTreeTable = this.getView().byId("TreeTableBasic");
+            oTreeTable.expandToLevel(1);
+        }
+    });
+    
+    
 
 });
